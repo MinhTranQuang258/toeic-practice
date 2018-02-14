bc5df3b2b909c3adaa3fcc9b7f51e179452980ee
MinhTranQuang258 <finalleo258@gmail.com>
2018-02-14 23:52:42 +0700

    Initialize a web application

---
+++ src/main/java/com/tqminh/vn/toeicpractice/ToeicPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.tqminh.vn.toeicpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeicPracticeApplication.class, args);
+	}
+}
