b52d7a859b9e46146d54a2300075b41871338bd1
MinhTranQuang258 <finalleo258@gmail.com>
2018-02-20 13:29:21 +0700

    Implement services of AccountDAO

--- src/main/java/com/tqminh/vn/toeicpractice/services/impl/AccountDAOImpl.java
+++ src/main/java/com/tqminh/vn/toeicpractice/services/impl/AccountDAOImpl.java
@@ -1,5 +1,8 @@
 package com.tqminh.vn.toeicpractice.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tqminh.vn.toeicpractice.common.Cache;
 import com.tqminh.vn.toeicpractice.common.Notification;
 import com.tqminh.vn.toeicpractice.model.Account;
 import com.tqminh.vn.toeicpractice.repositories.AccountWrapperRepository;
@@ -8,20 +11,24 @@ import com.tqminh.vn.toeicpractice.services.AccountDAO;
 
 public class AccountDAOImpl implements AccountDAO{
 
+	@Autowired
 	private AccountWrapperRepository accountWrapperRepository;
 	
-	@Override
+	@Autowired
+	private Cache cache;
+	
+  	@Override
 	public String loginAccount(Account account) {
 		try {
 			String userName= account.getUserName();
 			String password= account.getPassword();
 			if(userName.equals("admin") && password.equals("admin")) {
-				
+				cache.getMap().put(userName, account);
 			}
 			else {
 				AccountWrapper accountWrapper=  accountWrapperRepository.findAccountByUserAndPassword(userName, password);
 				if(accountWrapper != null) {
-					
+					cache.getMap().put(userName, account);
 				}
 				else {
 					return Notification.CAN_NOT_FIND_USER;
