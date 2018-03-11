79720b4b93c74d07dd8370334d22241610781de1
MinhTranQuang258 <finalleo258@gmail.com>
2018-03-11 15:27:20 +0700

    Refactor code

--- src/main/java/com/tqminh/vn/toeicpractice/controllers/AccountController.java
+++ src/main/java/com/tqminh/vn/toeicpractice/controllers/AccountController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AccountController {
-    @RequestMapping(value= "/login", method= RequestMethod.GET)
-    public String login() {
-        return "login";
-    }
 	
+//    @RequestMapping(value= "/login", method= RequestMethod.GET)
+//    public String login() {
+//        return "login";
+//    }
+//	
 }
