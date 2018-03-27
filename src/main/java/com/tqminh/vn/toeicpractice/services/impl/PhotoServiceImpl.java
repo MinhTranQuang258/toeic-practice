556d7718c375d73cc53c2bb3d01b0724e1ad9706
MinhTranQuang258 <finalleo258@gmail.com>
2018-03-27 17:43:37 +0700

    implements photo service to load photo question.

---
+++ src/main/java/com/tqminh/vn/toeicpractice/services/impl/PhotoServiceImpl.java
@@ -0,0 +1,33 @@
+/*
+ * Class: PhotoServiceImpl
+ *
+ * Created on Mar 27, 2018
+ *
+ * (c) Copyright Swiss Post Solution, unpublished work
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Swiss Post Solution.
+ * Floor 4-5-8, ICT Tower, Quang Trung Software City
+ */
+package com.tqminh.vn.toeicpractice.services.impl;
+
+import java.util.List;
+
+import com.tqminh.vn.toeicpractice.services.PhotoService;
+
+public class PhotoServiceImpl implements PhotoService{
+
+    @Override
+    public void loadFile() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<String> readFile() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
